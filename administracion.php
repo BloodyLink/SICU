6de71fa45cc6d567364633f02432d7cc6a677a27
BloodyLink <david.quezada.m@gmail.com>
2017-10-19 08:31:18 -0300

    Se comienza con front.

---
+++ administracion.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<head>
+    <title>.:SICU:.</title>
+</head>
+<body>
+<h2>Bienvenido al panel de administracion.</h2>
+<a href="index.php">Volver a Index</a>
+<br>
+<table>
+    <tr>
+        <td>
+            <!-- Agregar Colegio-->
+
+        </td>
+        <td>
+            <!-- Listar Colegios -->
+
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <!-- Agregar Curso-->
+
+        </td>
+        <td>
+            <!-- Listar Cursos -->
+
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <!-- Agregar Categoria-->
+
+        </td>
+        <td>
+            <!-- Listar Categorias -->
+
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <!-- Agregar Producto-->
+
+        </td>
+        <td>
+            <!-- Listar Productos -->
+
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <!-- Agregar Medio de pago-->
+
+        </td>
+        <td>
+            <!-- Listar Medios de pago -->
+
+        </td>
+    </tr>
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<head>
+<title>.:SICU:.</title>
+</head>
+<body>
+<h1>Sistema Centralizado Para Compra de Útiles Escolares (SICU)</h1>
+<hr>
+<ul>
+    <li><a href="administracion.php">Administracion</a></li>
+</ul>
+</body>
+</html>
